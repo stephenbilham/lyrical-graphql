178c6172926a55345ec28227b9e791531dabafa4
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-03-12 15:39:34 -0700

    add react-router specifically hashRouter

---
+++ client/components/SongList.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class SongList extends Component {
+	renderSongs() {
+		const { loading, songs } = this.props.data;
+
+		if (loading) {
+			return <div>loading...</div>;
+		}
+
+		return songs.map((song) => (
+			<ul key={song.id} className="collection">
+				<li className="collection-item">{song.title}</li>
+			</ul>
+		));
+	}
+
+	render() {
+		return <div>{this.renderSongs()}</div>;
+	}
+}
+
+const query = gql`
+	{
+		songs {
+			id
+			title
+		}
+	}
+`;
+
+export default graphql(query)(SongList);
